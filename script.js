325dde69b1727ac94343b36d9690dff089983d0a
Martin Guevara <martin_91@live.com.ar>
2023-05-19 15:04:08 -0300

    several improvements

--- script.js
+++ script.js
@@ -1,30 +1,28 @@
-const layer1 = document.getElementById('layer1')
-const layer2 = document.getElementById('layer2')
-const layer3 = document.getElementById('layer3')
-const layer4 = document.getElementById('layer4')
-const layer5 = document.getElementById('layer5')
-const layer6 = document.getElementById('layer6')
-const layer7 = document.getElementById('layer7')
+// SVG ASSIGNMENT
+for (let i = 1; i <= 7; i++) {
+    const element = document.getElementById(`layer${i}`)
+    element.style.backgroundImage = i === 7 ? `url("./src/images/c${i}.jpg")` : `url("./src/images/c${i}.svg")`
+    element.style.zIndex = 8 - i
+}
 
+// PARALLAX EVENT
 const clickableBox = document.getElementById('clickableBox')
 
 clickableBox.addEventListener('click', () => {
-    layer1.style.setProperty('top', '70px')
-    layer1.style.setProperty('transform', 'scale(1.1)')
-    layer2.style.setProperty('top', '70px')
-    layer2.style.setProperty('transform', 'scale(1.2)')
-    layer3.style.setProperty('top', '70px')
-    layer3.style.setProperty('transform', 'scale(1.3)')
-    layer4.style.setProperty('top', '70px')
-    layer4.style.setProperty('transform', 'scale(1.4)')
-    layer5.style.setProperty('top', '70px')
-    layer5.style.setProperty('transform', 'scale(1.5)')
-    layer6.style.setProperty('top', '70px')
-    layer6.style.setProperty('transform', 'scale(1.6)')
-    
-    layer7.style.setProperty('transform', 'scale(1.7)')  
+    // ZOOM EFFECT
+    for (let i = 1; i <= 7; i++) {
+        let element = document.getElementById(`layer${i}`)
+        element.style.setProperty('transform', `scale(1.${i})`)
+        i !== 7 && element.style.setProperty('top', '70px')
+    }
+
+    // FADE OUT
+    element = document.getElementById('textCotainer')
+    element.style.setProperty('transform', `scale(2)`)
+    element.style.animation = 'fadeout 0.7s forwards 0.2s'
 })
 
+// GLITCH EFFECT
 const glitchTrigger = ({ element, beforeOffset, afterOffset, opacity, content }) => {
     beforeOffset = beforeOffset || Math.floor((Math.random() * 10) + 20)
     afterOffset = afterOffset || Math.floor(Math.random() * 15)
@@ -37,9 +35,8 @@ const glitchTrigger = ({ element, beforeOffset, afterOffset, opacity, content })
     targetElement.style.setProperty('--glitch-before-left', beforeOffset + 'px')
 } 
 
-const glitch = document.getElementById("glitch")
 const paragraph = document.getElementById("paragraph")
-
+const glitch = document.getElementById("glitch")
 let timerDone = 0
 
 function timer(time) {
@@ -50,14 +47,14 @@ function timer(time) {
     setTimeout(() => glitchTrigger({ element: 'glitch', opacity: 0.4 }), 650)
     setTimeout(() => glitchTrigger({ element: 'glitch', beforeOffset: 15, afterOffset: 15, opacity: 0.8 }), 850)
 
-    glitchTrigger({ element: 'paragraph', opacity: 0.8, content: ' ' })
-    setTimeout(() => glitchTrigger({ element: 'paragraph', opacity: 0.4, content: ' ' }), 100)
-    setTimeout(() => glitchTrigger({ element: 'paragraph', opacity: 0.8, content: ' ' }), 150)
-    setTimeout(() => glitchTrigger({ element: 'paragraph', opacity: 1, content: ' ' }), 200)
-    setTimeout(() => glitchTrigger({ element: 'paragraph', opacity: 0.4, content: ' '  }), 650)
+    glitchTrigger({ element: 'paragraph', opacity: 0.8, content: '' })
+    setTimeout(() => glitchTrigger({ element: 'paragraph', opacity: 0.4, content: ' ', beforeOffset: Math.floor(Math.random() * 400), afterOffset: Math.floor(Math.random() * 400) }), 100)
+    setTimeout(() => glitchTrigger({ element: 'paragraph', opacity: 0.8, content: '', beforeOffset: Math.floor(Math.random() * 400), afterOffset: Math.floor(Math.random() * 400) }), 150)
+    setTimeout(() => glitchTrigger({ element: 'paragraph', opacity: 1, content: ' ', beforeOffset: Math.floor(Math.random() * 400), afterOffset: Math.floor(Math.random() * 400) }), 200)
+    setTimeout(() => glitchTrigger({ element: 'paragraph', opacity: 0.4, content: ''  }), 650)
     setTimeout(() => glitchTrigger({ element: 'paragraph', beforeOffset: 342, afterOffset: 342, opacity: 0.8, content: ' '  }), 850)
 
-    setTimeout(() =>  timerDone = 1, time);
+    setTimeout(() =>  timerDone = 1, time)
 }
 
 timer(1000)
@@ -71,4 +68,4 @@ setInterval(() => {
             timerDone = 0
         }
     }, initialTimeout)
-}, 1000);
+}, 1000)
