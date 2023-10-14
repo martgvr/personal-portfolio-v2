0fcd6f70df44f9318c6c64ce72c757fb0b7988c6
Martin Guevara <martin_91@live.com.ar>
2023-10-14 13:15:24 -0300

    about courses update

--- src/components/AboutEducation/AboutEducation.jsx
+++ src/components/AboutEducation/AboutEducation.jsx
@@ -1,63 +1,95 @@
-import './abouteducation.css'
+import "./abouteducation.css"
+import { useEffect, useState } from "react"
 
 import Reveal from "../../components/Reveal/Reveal"
-import AboutEducationYear from '../AboutEducationYear/AboutEducationYear'
+import AboutEducationCard from "../AboutEducationCard/AboutEducationCard"
 
 const coursesDone = [
-    {
-        year: 2022,
-		month: 'Enero',
-		title: 'Desarrollo Web',
-		description: 'asd asd',
-		institution: 'Coderhouse',
-		certificateURL: 'https://www.coderhouse.com/certificados/627c90a75aeb790019fa8454?lang=es',
-    },
 	{
-        year: 2022,
-		month: 'Febrero',
-		title: 'Javascript',
-		institution: 'Coderhouse',
-		description: 'asd asd',
-		certificateURL: 'https://www.coderhouse.com/certificados/627c90a75aeb790019fa8454?lang=es',
-    },
+		year: 2022,
+		month: "Enero",
+		title: "Desarrollo Web",
+		description:
+			"Adquiridos conocimientos relacionados con la creación de sitios web estáticos, el diseño web utilizando HTML y CSS, el uso de herramientas como GIT y SASS para el desarrollo web, la aplicación de Bootstrap en proyectos, la optimización para SEO, la implementación de sitios en servidores, y la gestión de clientes, incluyendo la presentación de presupuestos.",
+		institution: "Coderhouse",
+		certificateURL: "https://www.coderhouse.com/certificados/627c90a75aeb790019fa8454?lang=es",
+	},
 	{
-        year: 2022,
-		month: 'Marzo',
-		title: 'ReactJS',
-		institution: 'Coderhouse',
-		description: 'asd asd',
-		certificateURL: 'https://www.coderhouse.com/certificados/627c90a75aeb790019fa8454?lang=es',
-    },
+		year: 2022,
+		month: "Febrero",
+		title: "Javascript",
+		institution: "Coderhouse",
+		description:
+			"Adquiridos conocimientos en los fundamentos del lenguaje de programación más utilizado en la actualidad, con la capacidad de crear una amplia variedad de aplicaciones. Durante el curso, exploré las herramientas esenciales del lenguaje, analicé casos prácticos de aplicación y comprendí la utilidad de las librerías.",
+		certificateURL: "https://www.coderhouse.com/certificados/627c90a75aeb790019fa8454?lang=es",
+	},
 	{
-        year: 2023,
-		month: 'Abril',
-		title: 'Programación Backend',
-		institution: 'Coderhouse',
-		description: 'asd asd',
-		certificateURL: 'https://www.coderhouse.com/certificados/627c90a75aeb790019fa8454?lang=es',
-    },
+		year: 2022,
+		month: "Marzo",
+		title: "ReactJS",
+		institution: "Coderhouse",
+		description:
+			"Adquiridos conocimientos en la programación por componentes a través de JavaScript (JS) y ES6, además de comprender las ventajas de la utilización de flujos de datos. Durante el curso, apliqué el manejo de rutas utilizando Firebase y comprendí la utilización del virtual DOM a través de los desarrollos en React JS.",
+		certificateURL: "https://www.coderhouse.com/certificados/627c90a75aeb790019fa8454?lang=es",
+	},
 	{
-        year: 2023,
-		month: 'Mayo',
-		title: 'Desarrollo de aplicaciones',
-		institution: 'Coderhouse',
-		description: 'asd asd',
-		certificateURL: 'https://www.coderhouse.com/certificados/627c90a75aeb790019fa8454?lang=es',
-    },
+		year: 2023,
+		month: "Abril",
+		title: "Programación Backend",
+		institution: "Coderhouse",
+		description:
+			"Adquiridos conocimientos en el desarrollo de aplicaciones modernas utilizando Node.js y MongoDB. Programé en JavaScript en el lado del servidor, aplicando técnicas asincrónicas. Aprendí a trabajar con bases de datos NoSQL, dominando la gestión eficiente, ágil y altamente escalable de los datos.",
+		certificateURL: "https://www.coderhouse.com/certificados/627c90a75aeb790019fa8454?lang=es",
+	},
+	{
+		year: 2023,
+		month: "Mayo",
+		title: "Desarrollo de aplicaciones",
+		institution: "Coderhouse",
+		description:
+			"Adquiridos conocimientos y bases para crear aplicaciones bridge con capacidad de despliegue en Android e iOS, utilizando React Native y JavaScript. Te conectaste a APIs y adquiriste habilidades para la sincronización con Firebase o Realm DB.",
+		certificateURL: "https://www.coderhouse.com/certificados/627c90a75aeb790019fa8454?lang=es",
+	},
 ]
 
 export default function AboutEducation() {
-	const uniqueYears = [...new Set(coursesDone.map(course => course.year))]
+	const [yearSelected, setYearSelected] = useState(2022)
+	const [coursesToShow, setCoursesToShow] = useState([])
+
+	useEffect(() => {
+		const filteredCourses = coursesDone.filter((course) => course.year === yearSelected)
+		setCoursesToShow(filteredCourses)
+	}, [yearSelected])
 
 	return (
-		<div className='abouteducation__container flex-column'>
-			<Reveal>
-				<h1>Estudios realizados</h1>
+		<div className="abouteducation__container flex-column">
+			<div className="abouteducation__header flex-row">
+				<Reveal>
+					<h1>Estudios realizados</h1>
+				</Reveal>
+
+				<Reveal delay={0.5}>
+					<ul className="abouteducation__yearSelector flex-row">
+						<li className={yearSelected === 2022 ? 'active' : ''} onClick={() => setYearSelected(2022)}>
+							2022
+						</li>
+						<p>|</p>
+						<li className={yearSelected === 2023 ? 'active' : ''} onClick={() => setYearSelected(2023)}>2023</li>
+					</ul>
+				</Reveal>
+			</div>
+
+			<Reveal delay={0.8}>
+				<h2 style={{ color: "#eeabab" }}>{yearSelected}</h2>
 			</Reveal>
 
-			{
-				uniqueYears.map((year, index) => <Reveal slide delay={(index * 0.5) + 0.5}><AboutEducationYear year={year} data={coursesDone.filter(course => course.year === year)} /></Reveal>)
-			}
+			<div className="abouteducation__years flex-row">
+				{coursesToShow.map((course, index) => (
+					<Reveal delay={index * 0.2 + 1}>
+						<AboutEducationCard data={course} />
+					</Reveal>
+				))}
+			</div>
 		</div>
 	)
 }
--- src/components/AboutEducationCard/AboutEducationCard.jsx
+++ src/components/AboutEducationCard/AboutEducationCard.jsx
@@ -5,13 +5,17 @@ export default function AboutEducationCard({ data }) {
     const { month, title, institution, description, certificateURL } = data
 
     return (
-        <div className='abouteducationcard__container'>
-            <h2>{month}</h2>
-            <h3>{title}</h3>
-            <h4>{institution}</h4>
-            <p>{description}</p>
-            
-            <Button text={'Ver certificado'} link={certificateURL} />
+        <div className='abouteducationcard__container flex-column'>
+            <div>
+                <h2>{month}</h2>
+                <h3>{title}</h3>
+                <h4>{institution}</h4>
+                <p>{description}</p>
+            </div>
+
+            <div style={{ alignSelf: 'center' }}>
+                <Button text={'Ver certificado'} link={certificateURL} />
+            </div>
         </div>
     )
 }
